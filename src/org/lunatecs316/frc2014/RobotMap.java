be4b1812f8950662bd96af1a206d3aca259a389f
DomenicP <domenicpaul@me.com>
2014-01-14 14:34:51 -0500

    Add RobotMap class

---
+++ src/org/lunatecs316/frc2014/RobotMap.java
@@ -0,0 +1,30 @@
+package org.lunatecs316.frc2014;
+
+/**
+ * Mapping of robot IO ports to constants
+ * @author Domenic
+ */
+public class RobotMap {
+    
+    // PWM Output
+    public static final int kFrontLeftMotor = 1;
+    public static final int kRearLeftMotor = 2;
+    public static final int kFrontRightMotor = 3;
+    public static final int kRearRightMotor = 4;
+    
+    // Relay Output
+    public static final int kCompressorRelay = 1;
+    
+    // Solenoid Output
+    //public static final int kExampleSolenoid = 1;
+    
+    // Joystick Input
+    public static final int kDriverJoystick = 1;
+    public static final int kOperatorJoystick = 2;
+    
+    // Digital Input
+    public static final int kPressureSwitch = 1;
+    
+    // Analog Input
+    public static final int kGyro = 1;
+}
