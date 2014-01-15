c81948dc6c5bd6f4f3768b38761ca7099a3c2873
DomenicP <domenic316@icloud.com>
2014-01-15 12:54:17 -0500

    Add Gyro and Encoders to Drivetrain subsystem

--- src/org/lunatecs316/frc2014/subsystems/Drivetrain.java
+++ src/org/lunatecs316/frc2014/subsystems/Drivetrain.java
@@ -1,5 +1,8 @@
 package org.lunatecs316.frc2014.subsystems;
 
+import edu.wpi.first.wpilibj.CounterBase;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -10,20 +13,48 @@ import org.lunatecs316.frc2014.RobotMap;
  * @author 316Programming
  */
 public class Drivetrain {
+    // Drive Motors
     private Jaguar frontLeft = new Jaguar(RobotMap.kFrontLeftMotor);
     private Jaguar frontRight = new Jaguar(RobotMap.kFrontRightMotor);
     private Jaguar rearLeft = new Jaguar(RobotMap.kRearLeftMotor);
     private Jaguar rearRight = new Jaguar(RobotMap.kRearRightMotor);
     private RobotDrive driveMotors = new RobotDrive(frontLeft, rearLeft, frontRight, rearRight);
     
-    private Relay shiftingRelay = new Relay(RobotMap.kDrivetrainSolenoid);
+    // Shifter
+    private Relay shiftingRelay = new Relay(RobotMap.kShiftingRelay);
     
+    // Sensors
+    private Encoder leftEncoder = new Encoder(RobotMap.kLeftDriveEncoderA,
+                                              RobotMap.kLeftDriveEncoderB,
+                                              false,
+                                              CounterBase.EncodingType.k4X);
+    
+    private Encoder rightEncoder = new Encoder(RobotMap.kRightDriveEncoderA,
+                                               RobotMap.kRightDriveEncoderB,
+                                               false,
+                                               CounterBase.EncodingType.k4X);
+    
+    private Gyro gyro = new Gyro(RobotMap.kGyro);
+    
+    /**
+     * Default constructor
+     */
     public Drivetrain() {
     }
     
+    /**
+     * Initialize the subsystem
+     */
     public void init() {
         driveMotors.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
         driveMotors.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        
+        gyro.setSensitivity(0.007);
+        resetGyro();
+        
+        leftEncoder.start();
+        rightEncoder.start();
+        resetEncoders();
     }
     
     /**
@@ -47,5 +78,14 @@ public class Drivetrain {
      */
     public void shiftDown() {
         shiftingRelay.set(Relay.Value.kReverse);
+    }
+    
+    void resetGyro() {
+        gyro.reset();
+    }
+    
+    void resetEncoders() {
+        leftEncoder.reset();
+        rightEncoder.reset();
     }
 }
